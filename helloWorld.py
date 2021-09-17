5a603bf8191918ec344d8e296fd8684fac14576c
BoiL1 <boi.lipmann@outlook.com>
2021-09-17 09:54:56 +0200

    helloWorld

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+x = 6
+y = 8
+#waarvoor is die :
+if x < y:
+    print ("x is kleiner dan y")
+elif x == y:
+    print ("x is gelijk aan y")
+else:
+    print ("x is groter dan y")
+
+if x == 8 and y == 8:
+    print("allebij 8")
